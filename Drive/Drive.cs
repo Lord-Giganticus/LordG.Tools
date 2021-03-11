a3012be02b57af9410082de2c9a85ee979a95b92
Lord-Giganticus <mrubins64@gmail.com>
2021-03-11 13:10:19 -0600

    update project

---
+++ Drive/Drive.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+
+namespace LordG.Tools.Drive
+{
+    public class Drive
+    {
+        public class GetDrives
+        {
+            /// <summary>
+            /// Gets all drives that have a valid path.
+            /// </summary>
+            /// <returns>A string array of all drives that have a valid path.</returns>
+            public string[] AsString()
+            {
+                List<string> Drives = new List<string>();
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo d in drives)
+                {
+                    if (Directory.Exists(d.Name))
+                    {
+                        Drives.Add(d.Name);
+                    }
+                }
+                return Drives.ToArray();
+            }
+            /// <summary>
+            /// Gets all drives that have a valid path.
+            /// </summary>
+            /// <returns>A DriveInfo array of all drives that have a valid path.</returns>
+            public DriveInfo[] AsDriveInfo()
+            {
+                List<DriveInfo> Drives = new List<DriveInfo>();
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo d in drives)
+                {
+                    if (Directory.Exists(d.Name)) {
+                        Drives.Add(d);
+                    }
+                }
+                return Drives.ToArray();
+            }
+        }
+    }
+}
